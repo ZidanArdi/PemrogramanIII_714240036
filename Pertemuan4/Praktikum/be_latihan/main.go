4113c6b0e0f07767f788c046cbdfea215101e145
ZidanArdi <panjihadi6@gmail.com>
2026-04-11 23:17:09 +0700

    testing

---
+++ Pertemuan4/Praktikum/be_latihan/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+
+	"be_latihan/config"
+	"be_latihan/model"
+	"be_latihan/repository"
+
+	"github.com/gofiber/fiber/v2"
+	//"be_latihan/routes"
+)
+
+func main() {
+	app := fiber.New()
+
+	config.InitDB()
+	config.GetDB().AutoMigrate(&model.Mahasiswa{})
+
+	app.Get("/mahasiswa", func(c *fiber.Ctx) error {
+		data, err := repository.GetAllMahasiswa()
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.JSON(data)
+	})
+
+	app.Get("/mahasiswa/:npm", func(c *fiber.Ctx) error {
+		npm, err := strconv.ParseInt(c.Params("npm"), 10, 64)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "npm tidak valid"})
+		}
+
+		data, err := repository.GetMahasiswaByNPM(npm)
+		if err != nil {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.JSON(data)
+	})
+
+	app.Post("/mahasiswa", func(c *fiber.Ctx) error {
+		var mhs model.Mahasiswa
+		if err := c.BodyParser(&mhs); err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		data, err := repository.InsertMahasiswa(&mhs)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.Status(http.StatusCreated).JSON(data)
+	})
+
+	app.Put("/mahasiswa/:npm", func(c *fiber.Ctx) error {
+		npm, err := strconv.ParseInt(c.Params("npm"), 10, 64)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "npm tidak valid"})
+		}
+
+		var mhs model.Mahasiswa
+		if err := c.BodyParser(&mhs); err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		data, err := repository.UpdateMahasiswa(npm, &mhs)
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.JSON(data)
+	})
+
+	app.Delete("/mahasiswa/:npm", func(c *fiber.Ctx) error {
+		npm, err := strconv.ParseInt(c.Params("npm"), 10, 64)
+		if err != nil {
+			return c.Status(http.StatusBadRequest).JSON(fiber.Map{"error": "npm tidak valid"})
+		}
+
+		if err := repository.DeleteMahasiswa(npm); err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		return c.SendStatus(http.StatusNoContent)
+	})
+
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
+}
